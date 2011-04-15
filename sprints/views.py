d7409484baa75b8f7345e27a380eef482463a87e
Chleba <kwasar@seznam.cz>
2011-04-15 18:14:00 +0200

    graphs for past sprints

--- sprints/views.py
+++ sprints/views.py
@@ -51,8 +51,42 @@ def detail(req, sprint_id):
         #endwhile
     else:
         tickets = Tickets.objects.filter( Q(pub_date__lte=sprint.date_to) & (Q(devel_date__isnull=True) | Q(devel_date__lte=sprint.date_to, devel_date__gte=sprint.date_from)) & (Q(done_date__isnull=True) | Q(done_date__lte=sprint.date_to, done_date__gte=sprint.date_from)) )
-
-        pass
+        dates = []
+        todo = []
+        devel = []
+        done = []
+        df = sprint.date_from
+        addday = timedelta(days=1)
+        while df <= sprint.date_to:
+            todo_value = 0
+            devel_value = 0
+            done_value = 0
+            if df.isoweekday() not in (6,7):
+                dates.append(df)
+                for item in tickets:
+                    if item.done_date is not None and item.done_date <= df:
+                        done_value = done_value+1
+                    #endif
+                    if item.devel_date is not None and item.devel_date <= df:
+                        devel_value = devel_value+1
+                    #endif
+                    if item.pub_date <= df:
+                        todo_value = todo_value+1
+                    #endif
+                #endfor
+                done.append(done_value)
+                devel.append(devel_value)
+                todo.append(todo_value)
+            else:
+                done.append(done[len(done)-1])
+                devel.append(devel[len(devel)-1])
+                todo.append(todo[len(todo)-1])
+            #endif
+            df = df+addday
+            if df.date() == sprint.date_to.date():
+                break;
+            #endif
+        #endwhile
     #endif
 
     return render_to_response('sprints/detail.html', {
